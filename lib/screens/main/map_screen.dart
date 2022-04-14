b82069679a3bbf52a4b8ef557f13a65717b7ce49
swtmply <delosreyesjohnallen@gmail.com>
2022-04-14 22:07:19 +0800

    Update on map screen

--- lib/screens/main/map_screen.dart
+++ lib/screens/main/map_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:touristop/providers/user_location_provider.dart';
